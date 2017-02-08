3f050478dd7c9955ab53337fb5658d43aa1bf156
Eric Lee <ericlee@Erics-Air.Home>
2017-02-08 19:52:34 -0200

    Adds bootstrap and re-map view imports

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const hbs = require('hbs')
-
 const app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+app.use('/public/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/public/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/public/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.get('/', function(req, res) {
   res.render('index.hbs')
